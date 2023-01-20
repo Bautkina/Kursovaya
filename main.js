423e26391228bc68f6305f6df1ab72aafbcaec74
Bautkina <bautkina.anas@yandex.ru>
2023-01-20 23:00:47 +0300

    fix add sound (add noise category, curdate)

--- main.js
+++ main.js
@@ -45,8 +45,9 @@ $(document).ready(function(){
 
     document.getElementById("btn_add").onclick = function() {
         var Search = $('#add_sound').val();
+        var Noise = $('select').val();
         var Search_new = Search.replace(/\s/g, '!');
-        $("#map-view").load("mapAdd.php?id=" + Search_new);
-        console.log("dfdf");
+        var Noise_new =  Noise.replace(/\s/g, '!');
+        $(document).load("mapAdd.php?id=" + Search_new + "&noise=" + Noise_new);
     }
 })
